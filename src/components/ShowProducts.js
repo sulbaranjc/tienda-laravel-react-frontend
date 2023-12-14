66121d82465832b5979eee050ab891131c1a1e64
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-12-14 19:57:14 +0100

    Remove unnecessary test files and add dependencies

---
+++ src/components/ShowProducts.js
@@ -0,0 +1,22 @@
+import React, {useEffect, useState } from 'react'
+
+import axios from 'axios'
+
+import { Link } from 'react-router-dom'
+
+const ShowProducts = () => {
+  
+  const getAllProducts = () => {
+        
+  }
+
+  const deleteProduct = () => {
+
+  }
+
+  return (
+    <div>ShowProducts</div>
+  )
+}
+
+export default ShowProducts
